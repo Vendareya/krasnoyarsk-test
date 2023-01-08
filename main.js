cec1fac360f7ee64e8ea734069810205ffe1e5c4
Alena Bosx <alena-yarsk@yandex.ru>
2023-01-09 02:15:52 +0700

    tree 08.01.23

--- main.js
+++ main.js
@@ -34,9 +34,41 @@ const map = new Map({
     zoom: 11
   })
 });
+// const layerSwitcher = new ol.control.layerSwitcher({tipLabel: "Layers"});
+// map.addControl(layerSwitcher)
+// layerSwitcher.hidePanel = function () {};
+// layerSwitcher.showPanel();
+// map.getView().fit([])
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+for (let li of tree.querySelectorAll('li')) {
+  let span = document.createElement('span');
+  span.classList.add('show');
+  li.prepend(span);
+  span.append(span.nextSibling)
+}
+tree.onclick = function (event) {
+  console.log(event.target.tagName)
+  if (event.target.tagName != 'SPAN') return;
+
+  let childrenContainer = event.target.parentNode.querySelector('ul');
+  
+  if (!childrenContainer) return;
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+
+  if (childrenContainer.hidden) {
+    event.target.classList.add('hide');
+    event.target.classList.remove('show');
+  }
+  else {
+    event.target.classList.add('show');
+    event.target.classList.remove('hide');
+  }
+
+
+}
